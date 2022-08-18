dd733c5af95baea65f22169324b10c066d009401
dedikahfi <dedikahfirohmatullah@gmail.com>
2022-08-19 00:14:28 +0700

    add splash page

---
+++ lib/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const purpleColor = Color(0xff5843BE);
+const orangeColor = Color(0xffFF9376);
+const blackColor = Color(0xff000000);
+const whiteColor = Color(0xffFFFFFF);
+const grayColor = Color(0xff82868E);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: blackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: whiteColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w300,
+  color: grayColor,
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: purpleColor,
+);
